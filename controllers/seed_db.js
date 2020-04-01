f57b6306d72d597fb947b68d642d02372915f418
Abhik Banerjee <abhik.banerjee.1999@gmail.com>
2020-04-01 15:07:33 +0530

    working on SQL

--- controllers/seed_db.js
+++ controllers/seed_db.js
@@ -1,22 +1,26 @@
-let {Attendance, Classes} = require('../models');
+let {Attendance, Classes, sequelize} = require('../models');
 
 exports.seed_SQL_db = function(req,res){
      //URL body
-     body = req.body;     
-     Attendance.build({
-        schoolCode: body.icode,
-        teacherCode : body.tcode,
-        class : body.class,
-        section : body.sec,
-        subjectCode : body.subject,
-        date: body.date,
-        period: body.period
-     })
-     .save()
-     .then((ref)=>{
-         console.log(ref);
-         res.send(`${ref.dataValues} inserted!`);
-     });
+     body = req.body;  
+     sequelize.authenticate()
+     .then(res=>{
+        Attendance.build({
+            schoolCode: body.icode,
+            teacherCode : body.tcode,
+            class : body.class,
+            section : body.sec,
+            subjectCode : body.subject,
+            date: body.date,
+            period: body.period
+         })
+         .save()
+         .then((ref)=>{
+             console.log(ref);
+             res.send(`${ref.dataValues} inserted!`);
+         });
+     })   
+     
     //  Classes.build({
     //     schoolCode: body.icode,
     //     teacherCode : body.tcode,
